bc34c476a28f51fe2d479010a158d6484ba95f9a
Krishna <seandean125@gmail.com>
2025-06-08 14:14:10 -0400

    trying to merge

---
+++ structural/adapter.ts
@@ -0,0 +1,31 @@
+class AirConditioner {
+	public connectWifi(): void {
+		console.log('AC Connecting to wifi');
+	}
+	public startCooling(): void {
+		console.log('AC starting cooling');
+	}
+	public stopCooling(): void {
+		console.log('AC stopping cooling');
+	}
+	public disconnectWifi(): void {
+		console.log('AC disconnecting wifi');
+	}
+}
+
+class SmartLight {
+	public connectBluetooth(): void {
+		console.log('SmartLight Connecting to bluetooth');
+	}
+	public start(): void {
+		console.log('SmartLight starting ');
+	}
+	public stop(): void {
+		console.log('SmartLight stopping ');
+	}
+	public disconnectBluetooth(): void {
+		console.log('SmartLight disconnecting bluetooth');
+	}
+}
+
+// class
