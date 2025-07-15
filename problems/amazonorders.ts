65ddf98e6f8f186c7c441c85f9c767f941ec665c
Krishna <seandean125@gmail.com>
2025-07-15 02:49:58 -0400

    amazonorders question

---
+++ problems/amazonorders.ts
@@ -0,0 +1,17 @@
+// Amazon wants to build a system to compute few performance metrics for 3rd party sellers
+// based on their customer order delivery experience
+// Amazon customers want their orders delivered by the promised date and especially for
+// buisness customers, it is also important that the orders are delivered during open buisness
+// hours (Mon-Fri, 9am - 5pm)
+
+// Build a data model and implement a code logic to compute these performance metrics for an
+// input seller over all his orderService.
+// You can assume that the dataset for these data models will be populated for you offline
+// and you can call APIs to collect those dataset
+// Implement the system in such a way that you can initially compute the following two
+// performance metrics and ensure that it is flexible enough to add additional metrics later
+
+// *Timely-Delivery-Rate (TDR) measures the fraction of total orders that arrived before or on
+// promised delivery Date
+// *Open-Hour-Delivery-Rate (OHDR) measures the fraction of total orders that were delivered
+// during open business hours (Mon-Fri, 9am-5pm)
