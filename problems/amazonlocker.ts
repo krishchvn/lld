2417d47ea6add784f84a48fe6c3858dd59b9ca41
Krishna <seandean125@gmail.com>
2025-09-03 00:33:56 -0700

    amazonlocker incomplete

---
+++ problems/amazonlocker.ts
@@ -0,0 +1,97 @@
+enum SizeEnum {
+	SMALL,
+	MEDIUM,
+	LARGE,
+}
+
+class Locker {
+	public lockerId: number;
+	public size: SizeEnum;
+	public isAssigned: boolean;
+	public pin: number;
+
+	public constructor(lockerId: number, size: SizeEnum) {
+		this.lockerId = lockerId;
+		this.size = size;
+		this.pin = -1;
+	}
+
+	public assign(pin: number): void {
+		this.isAssigned = true;
+		this.pin = pin;
+	}
+	public unassign() {
+		this.isAssigned = false;
+		this.pin = -1;
+	}
+
+	public authenticatePin(pin: number) {
+		return this.isAssigned && this.pin === pin;
+	}
+}
+
+class Customer {
+	pin: number;
+	customerId: number;
+	latitude: number;
+	longitude: number;
+	assignedLocker: number;
+	public constructor(customerId: number, latitude: number, longitude: number) {
+		this.pin = -1;
+		this.customerId = customerId;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.assignedLocker = -1;
+	}
+
+	public update(lockerId: number, pin: number) {
+		this.assignedLocker = lockerId;
+		this.pin = pin;
+	}
+	public orderPackage(size: SizeEnum, amazonlocker: AmazonLockerInterface) {
+		// amazonlocker.assign_l;
+	}
+	public unAssignLocker(pin: number, locker: Locker): boolean {
+		if (this.assignedLocker !== -1 && locker.authenticatePin(pin)) {
+			this.assignedLocker = -1;
+			this.pin = -1;
+			console.log('Locker from ' + this.customerId + ' unassigned');
+			return true;
+		} else {
+			console.log('Some error while unassigning locker');
+			return false;
+		}
+	}
+}
+
+interface DistanceStrategy {
+	getDistance(latitude: number, longitude: number): number;
+}
+
+class EuclideanDistance implements DistanceStrategy {
+	public getDistance(latitude: number, longitude: number): number {
+		return (longitude ** 2 - latitude ** 2) ** 0.5;
+	}
+}
+
+interface AmazonLockerInterface {
+	instance: AmazonLocker;
+	getInstance();
+}
+
+class AmazonLocker {
+	private constructor() {}
+
+	private static instance: AmazonLocker;
+
+	public static getInstance() {
+		if (!AmazonLocker.instance) {
+			AmazonLocker.instance = new AmazonLocker();
+		}
+		return AmazonLocker.instance;
+	}
+
+	public findClosest() {}
+}
+
+const locker = AmazonLocker.getInstance();
